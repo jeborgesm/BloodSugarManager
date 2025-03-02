98b68c02d41ff2df9c7db5a796651e39d9d0ac96
Jaime Borges <jeborgesm@hotmail.com>
2025-03-01 23:31:43 -0600

    remove unused app component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
-import App from './App';
 import IndexPage from './IndexPage';
 import CarbTablePage from './CarbTablePage';
 import CarbTableFromJsonPage from './CarbTableFromJsonPage';
