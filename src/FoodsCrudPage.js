70080ae6641d3380ab6892e281b905292f33bbc3
Jaime Borges <jeborgesm@hotmail.com>
2025-03-10 22:05:20 -0500

    error fix

--- src/FoodsCrudPage.js
+++ src/FoodsCrudPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 function FoodsCrudPage() {
     const [foods, setFoods] = useState([]);
@@ -8,11 +8,11 @@ function FoodsCrudPage() {
     const [editFood, setEditFood] = useState({ product_name: '', carbohydrates_100g: '', serving_size: '' });
     const [paginationRange, setPaginationRange] = useState([1, 10]); // Initial range for pagination items
 
-    const fetchFoods = async () => {
+    const fetchFoods = useCallback(async () => {
         const response = await fetch(`/api/foods?query=${searchQuery}`);
         const data = await response.json();
         setFoods(data.foods);
-    };
+    }, [searchQuery]);
 
     useEffect(() => {
         fetchFoods();
