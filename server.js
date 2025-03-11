5d98ee0eceef17082ba0f6ecd6064211fb105601
Jaime Borges <jeborgesm@hotmail.com>
2025-03-10 22:38:12 -0500

    ensure that requests for static files are correctly handled before the catch-all route.

--- server.js
+++ server.js
@@ -85,8 +85,12 @@ app.delete('/api/foods/:productName', (req, res) => {
         });
 });
 
+// Serve static files from the build directory
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Catch-all route to serve index.html for all other requests
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/build/index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(PORT, () => {
