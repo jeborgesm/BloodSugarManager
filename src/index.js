9e447ccf89a2b39f8854909f982d342d54dbb9e8
Jaime Borges <jeborgesm@hotmail.com>
2025-03-11 22:35:03 -0500

    updated routing for react application

--- src/index.js
+++ src/index.js
@@ -1,23 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './pages/App'; // Importing the main App component
 import './index.css';
-import IndexPage from './pages/IndexPage';
-import CarbTablePage from './pages/CarbTablePage';
-import CarbTableFromJsonPage from './pages/CarbTableFromJsonPage';
-import FoodsCrudPage from './pages/FoodsCrudPage';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-root.render(
-    <Router>
-        <Routes>
-            <Route path="/" element={<IndexPage />} />
-            <Route path="/carbtable" element={<CarbTablePage />} />
-            <Route path="/carbtablefromjson" element={<CarbTableFromJsonPage />} />
-            <Route path="/foods" element={<FoodsCrudPage />} />
-        </Routes>
-    </Router>
-);
-
+root.render(<App />);
