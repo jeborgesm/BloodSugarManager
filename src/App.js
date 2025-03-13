e3a298d718a7c04b7d4dcbab69fc66082a37b33f
Jaime Borges <jeborgesm@hotmail.com>
2025-03-12 21:12:23 -0500

    Corrected paths to files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import IndexPage from './IndexPage';
-import CarbTablePage from './CarbTablePage';
-import CarbTableFromJsonPage from './CarbTableFromJsonPage';
-import FoodsCrudPage from './FoodsCrudPage';
+import IndexPage from './pages/IndexPage'; // Corrected import path
+import CarbTablePage from './pages/CarbTablePage';
+import CarbTableFromJsonPage from './pages/CarbTableFromJsonPage';
+import FoodsCrudPage from './pages/FoodsCrudPage';
 
 function App() {
     return (
@@ -20,3 +20,4 @@ function App() {
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './pages/App'; // Importing the main App component
+import App from './App'; // Corrected import path
 import './index.css';
 
 const container = document.getElementById('root');
